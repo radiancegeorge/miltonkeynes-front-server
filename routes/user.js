1b10f8d21505a4628297374156a836a87448997a
radiancegeorge <radiancegeorge@outlook.com>
2021-05-09 21:27:51 +0100

    all routes set

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const { Registration, login, index } = require('../controllers/users');
+const { Registration, login, index, genOtp, emailVerification, transaction, findUserForTransaction } = require('../controllers/users');
 const protect = require('../middleware/protect.middleware')
 const user = express.Router();
 
@@ -8,5 +8,9 @@ const user = express.Router();
 user.post('/', protect, index);
 user.post('/register', fileUpload(), Registration);
 user.post('/login', login);
+user.post('/generateOTP', genOtp);
+user.post('/verifyEmail', emailVerification)
+user.post('/transaction',protect, transaction)
+user.post('/findUser', protect, findUserForTransaction)
 
 module.exports = user;
